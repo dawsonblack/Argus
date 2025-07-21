b0b71da83d9e810243b38ad51531abfdd53a8d69
Dawson Black <leftbrainegg@Mac.attlocal.net>
2025-07-20 20:30:13 -0400

    Got volume bar on right side of card

--- lib/argus_web/live/appliance_live.ex
+++ lib/argus_web/live/appliance_live.ex
@@ -28,46 +28,48 @@ defmodule ArgusWeb.ApplianceLive do
   def render(assigns) do
     ~H"""
     <div id={"appliance-#{@appliance.slug}"} class={"appliance-card #{if @power == "on", do: "on", else: ""}"}>
-      <h2 class="appliance-title"><%= @appliance.name %></h2>
+      <div class="appliance-left">
+        <h2 class="appliance-title"><%= @appliance.name %></h2>
 
-      <div class="appliance-controls">
-        <%= if has_command?(@appliance.appliance_commands, "on") and has_command?(@appliance.appliance_commands, "off") do %>
-          <div class="switch-wrapper">
-            <span class="switch-label">Power</span>
-            <label class="switch">
-              <input
-                type="checkbox"
-                phx-click="toggle_power"
-                phx-target={@myself}
-                checked={@power == "on"}
-              />
-              <span class="slider"></span>
-            </label>
-          </div>
-        <% end %>
+        <div class="appliance-controls">
+          <%= if has_command?(@appliance.appliance_commands, "on") and has_command?(@appliance.appliance_commands, "off") do %>
+            <div class="switch-wrapper">
+              <span class="switch-label">Power</span>
+              <label class="switch">
+                <input
+                  type="checkbox"
+                  phx-click="toggle_power"
+                  phx-target={@myself}
+                  checked={@power == "on"}
+                />
+                <span class="slider"></span>
+              </label>
+            </div>
+          <% end %>
+        </div>
+      </div>
 
-        <div id="#{@appliance.slug}-volume-stack" class="volume-stack" phx-hook="VolumeHover">
-          <div class="volume-controls">
-            <div
-              class="volume-button plus"
-              phx-click="set_volume"
-              phx-value-value={min(@volume + 10, 100)}
-              phx-target={@myself}
-            >+</div>
+      <div id="#{@appliance.slug}-volume-stack" class="volume-stack" phx-hook="VolumeHover">
+        <div class="volume-controls">
+          <div
+            class="volume-button plus"
+            phx-click="set_volume"
+            phx-value-value={min(@volume + 10, 100)}
+            phx-target={@myself}
+          >+</div>
 
-            <div class="volume-line"></div>
+          <div class="volume-line"></div>
 
-            <div
-              class="volume-button minus"
-              phx-click="set_volume"
-              phx-value-value={max(@volume - 10, 0)}
-              phx-target={@myself}
-            >−</div>
-          </div>
+          <div
+            class="volume-button minus"
+            phx-click="set_volume"
+            phx-value-value={max(@volume - 10, 0)}
+            phx-target={@myself}
+          >−</div>
+        </div>
 
-          <div class="volume-bar-wrapper">
-            <div class="volume-bar-fill" style={"height: #{@volume}%"}></div>
-          </div>
+        <div class="volume-bar-wrapper">
+          <div class="volume-bar-fill" style={"height: #{@volume}%"}></div>
         </div>
       </div>
     </div>
